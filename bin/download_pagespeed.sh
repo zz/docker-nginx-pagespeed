6112128c162d6c2df59a2a2d8a650d71025a09dd
Fred Zhu <fred.zhu@alo7.com>
2018-02-23 09:56:39 +0800

    update to nginx 1.3.19

--- bin/download_pagespeed.sh
+++ bin/download_pagespeed.sh
@@ -4,7 +4,7 @@ echo "Downloading ngx_pagespeed ${NGINX_PAGESPEED_VERSION} from https://github.c
 
 wget --no-check-certificate -O - https://github.com/pagespeed/ngx_pagespeed/archive/${NGINX_PAGESPEED_VERSION}-${NGINX_PAGESPEED_RELEASE_STATUS}.tar.gz --progress=bar --tries=3 | tar zxf - -C /tmp
 
-PSOL_URL=$(cat "/tmp/ngx_pagespeed-${NGINX_PAGESPEED_VERSION}-${NGINX_PAGESPEED_RELEASE_STATUS}/PSOL_BINARY_URL")
+PSOL_URL=$(cat "/tmp/incubator-pagespeed-ngx-${NGINX_PAGESPEED_VERSION}-${NGINX_PAGESPEED_RELEASE_STATUS}/PSOL_BINARY_URL")
 
 # The size names must match install/build_psol.sh in mod_pagespeed
 if [ "$(uname -m)" = x86_64 ]; then
@@ -15,4 +15,5 @@ fi
 
 echo "Downloading ngx_pagespeed PSOL ${NGINX_PAGESPEED_VERSION} from ${PSOL_URL/\$BIT_SIZE_NAME/$PSOL_BIT_SIZE_NAME}..."
 
-wget --no-check-certificate -O - ${PSOL_URL/\$BIT_SIZE_NAME/$PSOL_BIT_SIZE_NAME} --progress=bar --tries=3 | tar zxf - -C /tmp/ngx_pagespeed-${NGINX_PAGESPEED_VERSION}-${NGINX_PAGESPEED_RELEASE_STATUS}
+wget --no-check-certificate -O - ${PSOL_URL/\$BIT_SIZE_NAME/$PSOL_BIT_SIZE_NAME} --progress=bar --tries=3 | tar zxf - -C /tmp/incubator-pagespeed-ngx-${NGINX_PAGESPEED_VERSION}-${NGINX_PAGESPEED_RELEASE_STATUS}
+ls /tmp/incubator-pagespeed-ngx-${NGINX_PAGESPEED_VERSION}-${NGINX_PAGESPEED_RELEASE_STATUS}
